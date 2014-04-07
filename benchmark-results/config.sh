b7b5f03412f722726acb283cfc4538e5bb500c35
Gabor Szarnyas <szarnyasg@gmail.com>
2014-04-07 14:21:44 +0200

    added benchmark result figures

---
+++ benchmark-results/config.sh
@@ -0,0 +1,10 @@
+export minSize=1                                                                                                                                                                                                                             
+export maxSize=512                                                                                                                                                                                                                           
+export tools="$tools neo4j-ramdisk fourstore fourstore-ramdisk"                                                                                                                                                                             
+export tools="fourstore-ramdisk"                                                                                                                                                                                                             
+export xmx=6G                                                                                                                                                                                                                                
+export scenarios="User XForm"                                                                                                                                                                                                                
+export timeout="15m"                                                                                                                                                                                                                         
+export verbose="false"                                                                                                                                                                                                                       
+export defaultArgs="-benchmarkMode true"                                                                                                                                                                                                     
+export series=5
