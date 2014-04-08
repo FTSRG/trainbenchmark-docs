3061493c9062f8bd107ab2c1c019639955e38a4a
Gabor Szarnyas <szarnyasg@gmail.com>
2014-04-08 17:08:58 +0200

    added script for compiling the wiki page of the Train Benchmark

---
+++ wiki/compile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget --output-document=trainbenchmark-temp.md https://opensourceprojects.eu/p/mondo/wiki/TrainBenchmark/raw
+cat trainbenchmark-temp.md | sed "s/^  / /" | sed ':a;N;$!ba;s/\n//g' | sed "s/\\\\r\\\\n/\\n/g" | sed "s/^     \*/    */" | tail -n +5 | head -n -1 > trainbenchmark.md
+pandoc trainbenchmark.md --output trainbenchmark.pdf --table-of-contents --variable=documentclass:article --variable=title:"Train Benchmark Guide"
