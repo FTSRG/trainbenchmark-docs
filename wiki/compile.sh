01bbfa27649fba1650fb43e8b3f55c20a562c858
Gabor Szarnyas <szarnyasg@gmail.com>
2014-04-08 18:50:31 +0200

    refactored run logic

--- wiki/compile.sh
+++ wiki/compile.sh
@@ -3,3 +3,4 @@
 wget --output-document=trainbenchmark-temp.md https://opensourceprojects.eu/p/mondo/wiki/TrainBenchmark/raw
 cat trainbenchmark-temp.md | sed "s/^  / /" | sed ':a;N;$!ba;s/\n//g' | sed "s/\\\\r\\\\n/\\n/g" | sed "s/^     \*/    */" | tail -n +5 | head -n -1 > trainbenchmark.md
 pandoc trainbenchmark.md --output trainbenchmark.pdf --table-of-contents --variable=documentclass:article --variable=title:"Train Benchmark Guide"
+pandoc trainbenchmark.md --output trainbenchmark.html
