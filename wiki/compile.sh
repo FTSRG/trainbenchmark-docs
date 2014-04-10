8b75c7ba3bd51b0a628c46ab56a4d3f61fefb668
Gabor Szarnyas <szarnyasg@gmail.com>
2014-04-10 11:43:45 +0200

    first whole result set

--- wiki/compile.sh
+++ wiki/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget --output-document=trainbenchmark-temp.md https://opensourceprojects.eu/p/mondo/wiki/TrainBenchmark/raw
-cat trainbenchmark-temp.md | sed "s/^  / /" | sed ':a;N;$!ba;s/\n//g' | sed "s/\\\\r\\\\n/\\n/g" | sed "s/^     \*/    */" | tail -n +5 | head -n -1 > trainbenchmark.md
-pandoc trainbenchmark.md --output trainbenchmark.pdf --table-of-contents --variable=documentclass:article --variable=title:"Train Benchmark Guide"
-pandoc trainbenchmark.md --output trainbenchmark.html --variable=title:"Train Benchmark Guide" --template=template.html
+wget --output-document=trainbenchmark-wiki-temp.md https://opensourceprojects.eu/p/mondo/wiki/TrainBenchmark/raw
+cat trainbenchmark-wiki-temp.md | sed "s/^  / /" | sed ':a;N;$!ba;s/\n//g' | sed "s/\\\\r\\\\n/\\n/g" | sed "s/^     \*/    */" | tail -n +5 | head -n -1 > trainbenchmark-wiki.md
+pandoc trainbenchmark-wiki.md --output trainbenchmark-wiki.pdf --table-of-contents --variable=documentclass:article --variable=title:"Train Benchmark Guide"
+pandoc trainbenchmark-wiki.md --output trainbenchmark-wiki.html --variable=title:"Train Benchmark Guide" --template=template.html
