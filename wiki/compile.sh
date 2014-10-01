a86a0f66055f7bef40795828c48c7e124e8508a6
Gabor Szarnyas <szarnyasg@gmail.com>
2014-10-01 09:19:43 +0200

    Updated release script

--- wiki/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget --output-document=trainbenchmark-wiki-temp.md https://opensourceprojects.eu/p/mondo/wiki/TrainBenchmark/raw
-cat trainbenchmark-wiki-temp.md | sed "s/^  / /" | sed ':a;N;$!ba;s/\n//g' | sed "s/\\\\r\\\\n/\\n/g" | sed "s/^     \*/    */" | tail -n +5 | head -n -1 > trainbenchmark-wiki.md
-pandoc trainbenchmark-wiki.md --output trainbenchmark-wiki.pdf --table-of-contents --variable=documentclass:article --variable=title:"Train Benchmark Guide"
-pandoc trainbenchmark-wiki.md --output trainbenchmark-wiki.html --table-of-contents --variable=title:"Train Benchmark Guide" --template=template.html
--- wiki/release.sh
+++ wiki/release.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-./compile.sh
-scp trainbenchmark-wiki.html trainbenchmark-wiki.pdf ../trainbenchmark-techreport/pdf/trainbenchmark-techreport.pdf szarnyasg@celeborn.mit.bme.hu:/var/www/trainbenchmark.inf.mit.bme.hu/doc
+scp ../trainbenchmark-techreport/pdf/trainbenchmark-techreport.pdf szarnyasg@celeborn.mit.bme.hu:/var/www/trainbenchmark.inf.mit.bme.hu/doc
